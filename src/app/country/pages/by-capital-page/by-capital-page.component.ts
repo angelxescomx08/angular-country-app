e04a4d6d22f4c72d5a3a9fb4549962c58f02b015
José Ángel Hernández Radilla <jose.angel.hdz.rda@gmail.com>
2025-08-19 11:36:07 -0600

    Refactor by-capital-page component to use resource for country search; handle errors and display results conditionally

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../types/country.type';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -17,13 +18,32 @@ export class ByCapitalPageComponent {
   isError = signal<string | null>(null);
   countries = signal<Country[]>([]);
 
-  onSearch(value: string) {
-    if (this.isLoading()) return;
-    this.isLoading.set(true);
-    this.isError.set(null);
-    this.countryService.searchByCapital(value).subscribe((resp) => {
-      this.isLoading.set(false);
-      this.countries.set(resp);
-    });
-  }
+  query = signal('');
+  countryResource = resource({
+    request: () => ({ query: this.query() }),
+    loader: async ({ request }) => {
+      if (!request.query) return [];
+
+      return await firstValueFrom(
+        this.countryService.searchByCapital(request.query)
+      );
+    },
+  });
+
+  // onSearch(value: string) {
+  //   if (this.isLoading()) return;
+  //   this.isLoading.set(true);
+  //   this.isError.set(null);
+  //   this.countryService.searchByCapital(value).subscribe({
+  //     next: (countries) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set(countries);
+  //     },
+  //     error: (error) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set([]);
+  //       this.isError.set(`No se encontró país con esa capital: ${value}`);
+  //     },
+  //   });
+  // }
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../types/rest-countries.type';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 import { CountryMapper } from '../mappers/country.mapper';
 
 const API_URL = 'https://restcountries.com/v3.1';
@@ -15,12 +15,15 @@ export class CountryService {
   searchByCapital(query: string) {
     query = query.toLocaleLowerCase();
 
-    return this.http
-      .get<RESTCountry[]>(`${API_URL}/capital/${query}`)
-      .pipe(
-        map((restCountries) =>
-          CountryMapper.mapRestCountryArrayToCountryArray(restCountries)
-        )
-      );
+    return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
+      map((restCountries) =>
+        CountryMapper.mapRestCountryArrayToCountryArray(restCountries)
+      ),
+      catchError((error) => {
+        return throwError(
+          () => new Error('No se pudo obtener países con esa query')
+        );
+      })
+    );
   }
 }
